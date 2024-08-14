e4763371dcfc8d5745cedf064fb41322cb2567b4
tondt <tondt@dyno.vn>
2024-08-15 01:02:45 +0700

    add

--- config/config.py
+++ config/config.py
@@ -2,7 +2,7 @@
 
 DB_HOST = 'localhost'        # Địa chỉ máy chủ MySQL (ví dụ: 'localhost' nếu chạy trên máy cục bộ)
 DB_USER = 'root'    # Tên đăng nhập MySQL của bạn
-DB_PASSWORD = '12345' # Mật khẩu MySQL của bạn
+DB_PASSWORD = '' # Mật khẩu MySQL của bạn
 DB_NAME = 'face_recognizer'    # Tên cơ sở dữ liệu bạn sử dụng
 MODEL_FACE_DETECTION = 'models/scr_face_detector.onnx'
 DATABASE = 'database/database.sqlite'
